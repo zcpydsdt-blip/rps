bebc8424214e68f6b79ba2c50463844efc2ca550
zcpydsdt-blip <zcpydsdt@gmail.com>
2025-09-24 04:44:33 -0400

    tweak syntax

--- main.py
+++ main.py
@@ -31,8 +31,8 @@ def main():
         if user_choice == computer_choice:
             print("It's a tie!")
         elif (user_choice == 'Rock' and computer_choice == 'Scissors') or \
-             (user_choice == 'Paper' and computer_choice == 'Rock') or
-                (user_choice == 'Scissors' and computer_choice == 'Paper'):
+             (user_choice == 'Paper' and computer_choice == 'Rock') or \
+             (user_choice == 'Scissors' and computer_choice == 'Paper'):
             print("You win!")
             user_wins += 1
         else:
